39aec6670fd3e57f914a40b2d0a24223d48f71d4
gmillia <46548986+gmillia@users.noreply.github.com>
2019-08-09 22:33:51 -0700

    updates

--- public/Dashboard/JS/loadSaveMenu.js
+++ public/Dashboard/JS/loadSaveMenu.js
@@ -5,10 +5,12 @@ File controls behavior of te Save subpage
 */
 
 /*
-Function that controls behavior on the Save button click: opens Save submenu
+PURPOSE: Function that controls behavior on the Save button click
+INVOKED: On the Save button click in the header submenu
 */
 $(document).on('click', '.saveBtn', function()
 {
+    //Load the content of the save menu if its not already open
     if(!$("#saveName")[0])
     {
         $(".Content").hide();
@@ -17,36 +19,15 @@ $(document).on('click', '.saveBtn', function()
 });
 
 /*
-Helper function that controls behavior when Save button is clicked in the Save-submenu (when user wants to save data)
-
-TODO: alert user if file already exists uder certain name + limit number of charactes for filename
+PURPOSE: Helper function that controls behavior when Save button is clicked in Save menu
+INVOKED: On the Save button click in the save submenu
 */
 $(document).on('click', '#saveGame', function()
 {
-    //console.log("Save");
     const db = firebase.firestore();
 
-    //Grab the names of the teams
-    var homeTeam = $(".t1-name-input")[0].value;
-    var awayTeam = $(".t2-name-input")[0].value;
-
-    //If empty save as home/away team for names
-    if(homeTeam == "") homeTeam = "Home Team";
-    if(awayTeam == "") awayTeam = "Away Team";
-
-    var date = getDate();
-    //Data to be saved is user id (for matching and making sure access is grandted only to auth users) and teams info
-    var data = 
-    {
-        uid: firebase.auth().currentUser.uid,
-        date: date,
-        t1: T1,
-        t2: T2
-    }
-
-    //Grab the desired save file name, save as Temporary is name is empty
-    var filename = $("#saveName")[0].value;
-    if(filename == "") filename = "Temporary";
+    var data = getData();
+    var filename = getFilename();
 
     var docRef = db.collection("Games").doc(filename);
     
@@ -61,28 +42,94 @@ $(document).on('click', '#saveGame', function()
         }
     }).catch(function()
     {
-        $("#saveOverlay")[0].innerHTML = "<span>" + "Saved as: " + filename + "</span>";
-        $("#saveOverlay")[0].style.display = "flex";
-        setTimeout(function()
-        {
-            $("#saveOverlay").fadeOut(800);
-        }, 2000);
+        //file doesn't exist, errors out -> save
+        var overlayText = "<span>" + "Saved as: " + filename + "</span>";  //text displayed when saved
+        saveGame(filename, data, overlayText);
     });
-    /*
-    //Save data in the Games collection with the desired filename
-    db.collection("Games").doc(filename).set(JSON.parse(JSON.stringify(data))).then(function()
-    {
-        $("#saveOverlay")[0].innerHTML = "<span>" + "Saved as: " + filename + "</span>";
-        //$("#saveOverlay").show();
-        $("#saveOverlay")[0].style.display = "flex";
-        setTimeout(function()
-        {
-            $("#saveOverlay").fadeOut(800);
-        }, 2000);
-    });
-    */
 });
 
+/*
+PURPOSE: Helper function that saves the game and displays success overlay
+INVOKED: When the user decides to save/override file
+*/
+function saveGame(filename, data, overlayText)
+{
+    var db = firebase.firestore();
+    data = JSON.parse(JSON.stringify(data));  //create savable data
+    db.collection("Games").doc(filename).set(data).then(function()
+    {
+        $("#saveOverlay")[0].innerHTML = overlayText;  //text displayed
+        $("#saveOverlay")[0].style.display = "flex";  //display the overlay
+
+        //Fade out the overlay
+        setTimeout(function()
+        {
+            $("#saveOverlay").fadeOut(800);
+        }, 2000); 
+    }); 
+}
+
+/*
+PURPOSE: Override the saved file with new data
+INVOKED: On the Yes click when override is asked
+*/
+$(document).on('click', '#override', function()
+{
+    $("#failOverlay").hide();  //hide fail overlay that asks about override
+    $("#overrideContainer").hide();  //hide override buttons (yes/no)
+    $("#saveContainer").show();  //display the Save button again
+
+    var overlayText = "<span>" + filename + " overridden" + "</span>";  //text displayed in the successful override
+    saveGame(getFilename(), getData(), overlayText);
+});
+
+/*
+PURPOSE: Hide override stuff
+INVOKED: On the No click when override is asked
+*/
+$(document).on('click', '#notOverride', function()
+{
+    $("#failOverlay").fadeOut(800);  //hide failed overlay
+    $("#overrideContainer").hide();  //hide override buttons
+    $("#saveContainer").show();  //display Save button
+});
+
+/*
+PURPOSE: get data to be saved
+INVOKED: whenever we need data to save (in override or regular save)
+*/
+function getData()
+{
+    var date = getDate();
+    //Data to be saved is user id (for matching and making sure access is grandted only to auth users) and teams info
+    var data = 
+    {
+        uid: firebase.auth().currentUser.uid,
+        date: date,
+        t1: T1,
+        t2: T2
+    }
+
+    return data;
+}
+
+/*
+PURPOSE: Helper function that gets the name of the filename user wants to save the data (under)
+INVOKED: Whenever we need the name of the file (when saving or overriding)
+*/
+function getFilename()
+{
+    //Grab the desired save file name, save as Temporary is name is empty
+    var filename = $("#saveName")[0].value;
+    if(filename == "") filename = "Temporary";
+
+    return filename;
+}
+
+/*
+PURPOSE: get todays date in the form DD/MM/YYYY
+INVOKED: Whenever we need the data (when saving or overriding)
+*/
 function getDate()
 {
     var today = new Date();
